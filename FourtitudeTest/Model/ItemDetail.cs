6ed0a8be4c7e7725a9f252d91c6fd90c72de0dd1
Aiman <42263487+41m4n@users.noreply.github.com>
2025-04-24 11:39:19 +0800

    intial commit

---
+++ FourtitudeTest/Model/ItemDetail.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FourtitudeTest.Model
+{
+    public class ItemDetail
+    {
+        [ForeignKey(nameof(Partner))]
+        public string companyDbmId { get; set; }
+        public Partner partner { get; set; }
+        public string name { get; set; }
+        public int qty { get; set; }
+        public long unitPrice { get; set; }
+    }
+}
